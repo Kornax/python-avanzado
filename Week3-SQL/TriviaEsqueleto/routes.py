8e4395628a89b1a64bd8201f43a16ade4619c419
Kornax <jeabreugentini@gmail.com>
2020-06-02 20:58:31 +0200

    'sesiones'

--- Week3-SQL/TriviaEsqueleto/routes.py
+++ Week3-SQL/TriviaEsqueleto/routes.py
@@ -1,19 +1,25 @@
 # importamos la instancia de Flask (app)
 from apptrivia import app
 import random
+import datetime
 
 # importamos los modelos a usar
 from models.models import Categoria, Pregunta, Respuesta
 
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+
 
 @app.route('/trivia')
+@app.route('/')
 def index():
-    return "<h2>hola Trivia</h2>"
+    session.clear()
+    return "<h2>hola Trivia</h2><a href=\"/trivia/categorias\">Entrar<a>"
 
 
 @app.route('/trivia/categorias', methods=['GET'])
 def mostrarcategorias():
+    if 'startTime' not in session:
+        session['startTime'] = datetime.datetime.now()
     categorias = Categoria.query.all()
     return render_template('categorias.html', categorias=categorias)
 
@@ -29,6 +35,17 @@ def mostrarpregunta(id_categoria):
 
 @app.route('/trivia/<int:id_categoria>/<int:id_pregunta>/respuesta/<int:id_respuesta>', methods=['GET'])
 def mostrarRespuesta(id_categoria, id_pregunta, id_respuesta):
-    # respuesta = Pregunta.query.filter_by(categoria_id=id_categoria)
-    respuesta = Respuesta.query.filter_by(id=id_respuesta)
-    return render_template('respuesta.html' , respuesta=respuesta)
+    respuesta = Respuesta.query.filter_by(id=id_respuesta)[0]
+    if str(id_categoria) not in session:
+        session[str(id_categoria)] = respuesta.answer
+    categorias = [str(c.id) for c in Categoria.query.all()]
+    categoriasAnswers = [session[i] for i in categorias if i in session]
+    if all(categoriasAnswers) and len(categorias) == len(categoriasAnswers) :
+        return redirect(url_for('winner'))
+    else:
+        return render_template('respuesta.html' , respuesta=respuesta)
+
+@app.route('/trivia/winner')
+def winner():
+    time = datetime.datetime.now() - session['startTime']
+    return render_template('winner.html' , time = time)
